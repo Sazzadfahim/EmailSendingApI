e31c082893f7e1f5b8f585498afcbd8377663506
sazzad <sazzadfahim4@gmail.com>
2022-10-08 00:59:30 +0600

    Add project files.

---
+++ EmailSendingApp/Controllers/EmailSendingController.cs
@@ -0,0 +1,30 @@
+﻿using EmailSendingApp.Services;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
+namespace EmailSendingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailSendingController : ControllerBase
+    {
+        private readonly EmailService _emailService;
+        public EmailSendingController(EmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            _emailService.SendEmail(request);
+
+
+            return Ok();
+        }
+    }
+
+}
---
+++ EmailSendingApp/Services/EmailService.cs
@@ -0,0 +1,38 @@
+﻿using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+
+namespace EmailSendingApp.Services
+{
+    public class EmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        public void SendEmail(EmailDto request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("keven61@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(TextFormat.Plain)
+            {
+                Text = request.Body
+            };
+
+            using (var smtp = new MailKit.Net.Smtp.SmtpClient())
+            {
+                smtp.Connect(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+                smtp.Authenticate(_config.GetSection("EmailUsername").Value, _config.GetSection("EmailPassword").Value);
+                smtp.Send(email);
+                smtp.Disconnect(true);
+
+            }
+        }
+    } 
+}
